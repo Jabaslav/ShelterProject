45d64cf0823f5b675510351721263230b8e09426
Jabaslav <revigamingplay@gmail.com>
2025-04-14 21:39:15 +0400

    Initial commit

---
+++ src/test/java/ru/shelter/ShelterProject/ShelterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shelter.ShelterProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
